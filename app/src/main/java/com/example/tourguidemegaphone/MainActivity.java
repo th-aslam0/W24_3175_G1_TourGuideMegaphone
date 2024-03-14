1e7df72420d88920fc32e6e97933d2b0d94b3982
Leandro <berazal@student.douglascollege.ca>
2024-03-13 22:05:59 -0700

    Sign up activity layout

--- app/src/main/java/com/example/tourguidemegaphone/MainActivity.java
+++ app/src/main/java/com/example/tourguidemegaphone/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.tourguidemegaphone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,12 +23,21 @@ public class MainActivity extends AppCompatActivity {
         //
 
         TextView txtViewSignUp = findViewById(R.id.txtSignUpInstead);
+        EditText editTxtEmail = findViewById(R.id.editTextEmailAddress);
         SpannableString spannableSignUp = new SpannableString(txtViewSignUp.getText());
+
 
         ClickableSpan clickableSpan = new ClickableSpan() {
             @Override
             public void onClick(View widget) {
-                Toast.makeText(MainActivity.this, "TO-DO: Open Sign Up activity.", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(MainActivity.this, "TO-DO: Open Sign Up activity.", Toast.LENGTH_SHORT).show();
+                Bundle bundle = new Bundle();
+                bundle.putString("EMAIL", editTxtEmail.getText().toString());
+                Intent intent = new Intent
+                        (MainActivity.this,SignUpActivity.class);
+                intent.putExtras(bundle);
+
+                startActivity(intent);
             }
         };
 
