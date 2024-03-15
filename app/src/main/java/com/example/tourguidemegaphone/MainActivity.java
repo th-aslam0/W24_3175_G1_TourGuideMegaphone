f29767fa7e9ddaadc1abe5f2053c30d76f46fdae
lbrza <152647424+lbrza@users.noreply.github.com>
2024-03-14 18:46:07 -0700

    Main login activity (#3)
    * I added a splash screen with a megaphone icon to the project.
    
    * Main log in or sign up activity

--- app/src/main/java/com/example/tourguidemegaphone/MainActivity.java
+++ app/src/main/java/com/example/tourguidemegaphone/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.tourguidemegaphone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +18,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //
+
+        TextView txtViewSignUp = findViewById(R.id.txtSignUpInstead);
+        SpannableString spannableSignUp = new SpannableString(txtViewSignUp.getText());
+
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View widget) {
+                Toast.makeText(MainActivity.this, "TO-DO: Open Sign Up activity.", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        // setting a link to "sign up" part.
+        spannableSignUp.setSpan(clickableSpan, 23, 30, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        txtViewSignUp.setText(spannableSignUp);
+        txtViewSignUp.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
