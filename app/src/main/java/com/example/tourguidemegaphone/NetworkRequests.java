029951859fa7ee112623eb569d797538badc50ba
th-aslam0 <158779728+th-aslam0@users.noreply.github.com>
2024-03-31 01:45:54 +0500

    Implemented SignUp & Login with backend network calls (#7)
    * Added network request for login
    
    * updated url
    
    * added signup and login API
    
    ---------
    
    Co-authored-by: th-aslam <tamoor.h.aslam@ooma.com>

---
+++ app/src/main/java/com/example/tourguidemegaphone/NetworkRequests.java
@@ -0,0 +1,23 @@
+package com.example.tourguidemegaphone;
+import com.google.gson.annotations.SerializedName;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public class NetworkRequests {
+    private String BASEURL = "";
+
+    public NetworkRequests(String BASEURL) {
+
+    }
+
+    public static void signUp(String email, String password){
+
+    }
+    public static void login(String email, String password, String firstName, String lastName){
+
+    }
+
+
+}
+
