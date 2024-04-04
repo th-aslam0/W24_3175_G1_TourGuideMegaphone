2a5b2e49466942ca15e67b663e9623ccd0db230e
th-aslam <tamoor.h.aslam@ooma.com>
2024-04-04 00:05:01 -0700

    Tour Sessions List continued

---
+++ app/src/main/java/com/example/tourguidemegaphone/TourModel.java
@@ -0,0 +1,80 @@
+package com.example.tourguidemegaphone;
+
+public class TourModel {
+
+        String tourTitle;
+        String tourCountry;
+        String tourCity;
+        String tourDescription;
+        String tourStartTime;
+        String tourDuration;
+        double tourPrice;
+
+        public TourModel(String tourTitle, String tourCountry, String tourCity, String tourDescription, String tourStartTime, String tourDuration, double tourPrice) {
+            this.tourTitle = tourTitle;
+            this.tourCountry = tourCountry;
+            this.tourCity = tourCity;
+            this.tourDescription = tourDescription;
+            this.tourStartTime = tourStartTime;
+            this.tourDuration = tourDuration;
+            this.tourPrice = tourPrice;
+        }
+
+        public String getTourTitle() {
+            return tourTitle;
+        }
+
+        public void setTourTitle(String tourTitle) {
+            this.tourTitle = tourTitle;
+        }
+
+        public String getTourCountry() {
+            return tourCountry;
+        }
+
+        public void setTourCountry(String tourCountry) {
+            this.tourCountry = tourCountry;
+        }
+
+        public String getTourCity() {
+            return tourCity;
+        }
+
+        public void setTourCity(String tourCity) {
+            this.tourCity = tourCity;
+        }
+
+        public String getTourDescription() {
+            return tourDescription;
+        }
+
+        public void setTourDescription(String tourDescription) {
+            this.tourDescription = tourDescription;
+        }
+
+        public String getTourStartTime() {
+            return tourStartTime;
+        }
+
+        public void setTourStartTime(String tourStartTime) {
+            this.tourStartTime = tourStartTime;
+        }
+
+        public String getTourDuration() {
+            return tourDuration;
+        }
+
+        public void setTourDuration(String tourDuration) {
+            this.tourDuration = tourDuration;
+        }
+
+        public double getTourPrice() {
+            return tourPrice;
+        }
+
+        public void setTourPrice(double tourPrice) {
+            this.tourPrice = tourPrice;
+        }
+    }
+
+
---
+++ app/src/main/java/com/example/tourguidemegaphone/ViewTourGuideSessionsActivity.java
@@ -0,0 +1,24 @@
+package com.example.tourguidemegaphone;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ViewTourGuideSessionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_view_tour_guide_sessions);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
